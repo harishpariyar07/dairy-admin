05e2a9dec0291497612239d2f1f4ac099461433b
loki <uddeeptaraajkashyap@gmail.com>
2023-10-12 11:08:51 +0530

    modification: bill template

--- utils/billTemplate.js
+++ utils/billTemplate.js
@@ -1,5 +1,86 @@
 import formatDate from "./formatDate";
 
+
+
+const htmlTemplateShort = (
+  farmerId,
+  farmerName,
+  startDate,
+  endDate,
+  filteredTableData,
+  avgFat,
+  avgSnf,
+  totalQty,
+  totalCredit,
+  totalDebit
+) => {
+  return `
+    <html>
+      <head>
+        <style>
+          /* Add your CSS styles here */
+          table {
+            border-collapse: collapse;
+            width: 100%;
+            border: 1px solid black;
+          }
+          th, td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: center;
+          }
+        </style>
+      </head>
+      <body>
+        <p style="text-align: center"><u>Farmer ID:</u> ${farmerId} <u>Farmer Name:</u> ${farmerName}</p>
+        <p style="text-align: center">From ${formatDate(startDate)} To ${formatDate(endDate)}</p>
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Shift</th>
+              <th>Fat</th>
+              <th>Snf</th>
+              <th>Qty</th>
+              <th>Credit</th>
+              <th>Debit</th>
+              <th>Remarks</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${filteredTableData
+              .map(
+                (item) => `
+                  <tr>
+                    <td>${item[1] || '-'}</td>
+                    <td>${item[2] === "Morning" ? "M" : "E" || '-'}</td>
+                    <td>${item[3] || '-'}</td>
+                    <td>${item[4] || '-'}</td>
+                    <td>${item[5] || '-'}</td>
+                    <td>${item[6] || '-'}</td>
+                    <td>${item[7] || '-'}</td>
+                    <td>${item[8] || ''}</td>
+                  </tr>
+                `
+              )
+              .join('')}
+            <tr>
+              <th>-</th>
+              <th>-</th>
+              <th>${avgFat}</th>
+              <th>${avgSnf}</th>
+              <th>${totalQty}L</th>
+              <th>${totalCredit}</th>
+              <th>${totalDebit}</th>
+              <th>-</th>
+            </tr>
+          </tbody>
+        </table>
+      </body>
+    </html>
+  `;
+};
+
 const htmlTemplate = (
   organizationName,
   address,
@@ -97,4 +178,4 @@ const htmlTemplate = (
   `;
 };
 
-export default htmlTemplate;
+export { htmlTemplate, htmlTemplateShort}
